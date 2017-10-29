2e1e331e54a8fd71f8a5dd6071526ad2900991b5
Wahid Tanner <wahidtanner@gmail.com>
2017-10-29 00:21:51 -0400

    Initial Commit

---
+++ aboqa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aboqa
+//
+//  Created by Abdul Wahid Tanner on 10/29/17.
+//  Copyright © 2017 Take Up Code. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
